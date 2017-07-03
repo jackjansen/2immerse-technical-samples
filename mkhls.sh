e51be29dba07124db8af1511f9df99e4cc234128
Jack Jansen <Jack.Jansen@cwi.nl>
2017-07-03 15:32:25 +0200

    Fixed script to produce HLS somewhat similar to what the dash manifest uses

--- mkhls.sh
+++ mkhls.sh
@@ -3,13 +3,26 @@
 # Partially discovered from google "ffmpeg hls" and from
 # https://superuser.com/questions/875579/how-to-generate-multiple-bitrate-hls-content-from-live-rtsp-stream
 #
-mkdir $1.hls
-ffmpeg -i $1.mp4 -f hls $1.hls/$1.m3u8
-# echo "#extm3U" > $1.hls/$1.m3u8
-# rate=???
-# width=???
-# height=???
-# ffmpeg -i $1 -f hls -maxrate $rate -s ${width}x${height} $1.hls/$1-${rate}-${width}-${height}.m3u8
-# echo "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=${rate}" >> $1.hls/$1.m3u8
-# echo "$1-${rate}-${width}-${height}.m3u8" >> $1.hls/$1.m3u8
-# echo 
+if [ -d ${1}.hls ]; then 
+	rm -rf ${1}.hls
+fi
+mkdir ${1}.hls
+ffmpeg  -i ${1}.mp4 \
+	-f hls -profile:v baseline -level 3.0 -hls_time 4 -start_number 1 -hls_list_size 999999 -s 1920x1080 -maxrate 6711536 ${1}.hls/${1}_6711536_.m3u8 \
+	-f hls -profile:v baseline -level 3.0 -hls_time 4 -start_number 1 -hls_list_size 999999 -s 426x240 -maxrate 832226 ${1}.hls/${1}_832226_.m3u8 \
+	-f hls -profile:v baseline -level 3.0 -hls_time 4 -start_number 1 -hls_list_size 999999 -s 640x360 -maxrate 1075624 ${1}.hls/${1}_1075624_.m3u8 \
+	-f hls -profile:v baseline -level 3.0 -hls_time 4 -start_number 1 -hls_list_size 999999 -s 854x480 -maxrate 1863384 ${1}.hls/${1}_1863384_.m3u8 \
+	-f hls -profile:v baseline -level 3.0 -hls_time 4 -start_number 1 -hls_list_size 999999 -s 1280x720 -maxrate 3690962 ${1}.hls/${1}_3690962_.m3u8 
+cat > ${1}.hls/${1}.m3u8 << xyzzy
+#extm3U
+#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=6711536
+${1}_6711536_.m3u8
+#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=832226
+${1}_832226_.m3u8
+#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=1075624
+${1}_1075624_.m3u8
+#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=1863384
+${1}_1863384_.m3u8
+#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=3690962
+${1}_3690962_.m3u8
+xyzzy
