393f100e033df6c870d4ee1c290eb421be837edc
Jack Jansen <Jack.Jansen@cwi.nl>
2017-07-04 14:30:15 +0200

    Added 2 HLS video examples

--- mkhls.sh
+++ mkhls.sh
@@ -14,9 +14,7 @@ ffmpeg  -i ${1}.mp4 \
 	-f hls -profile:v baseline -level 3.0 -hls_time 4 -start_number 1 -hls_list_size 999999 -s 854x480 -maxrate 1863384 ${1}.hls/${1}_1863384_.m3u8 \
 	-f hls -profile:v baseline -level 3.0 -hls_time 4 -start_number 1 -hls_list_size 999999 -s 1280x720 -maxrate 3690962 ${1}.hls/${1}_3690962_.m3u8 
 cat > ${1}.hls/${1}.m3u8 << xyzzy
-#extm3U
-#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=6711536
-${1}_6711536_.m3u8
+#EXTM3U
 #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=832226
 ${1}_832226_.m3u8
 #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=1075624
@@ -25,4 +23,6 @@ ${1}_1075624_.m3u8
 ${1}_1863384_.m3u8
 #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=3690962
 ${1}_3690962_.m3u8
+#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=6711536
+${1}_6711536_.m3u8
 xyzzy
